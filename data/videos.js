b4d8448fd2c1ca21a108e2582938be9e832431a2
Tokmando <98414437+Tokmando@users.noreply.github.com>
2025-06-14 01:44:40 +0400

    Update videos.js

--- data/videos.js
+++ data/videos.js
@@ -1,30 +1,30 @@
 const videos = [
   {
     url: "https://www.youtube.com/watch?v=Sge5Y-G8Jds&t=382s",
-    title: "Sky News: Georgia’s new president sparks protests",
-    publication: "Sky News",
-    date: "2024-05-20",
+    title: "Russian flight from Ukraine to Georgia",
+    publication: "YouTube",
+    date: "2023-03-02",
     image: "assets/video1.jpg"
   },
   {
-    url: "https://www.ekhokavkaza.com/a/%d1%83%d0%ba%d1%80%d0%b0%d0%b8%d0%bd%d0%b0-%d1%80%d0%be%d1%81%d1%81%d0%b8%d1%8f-%d0%b3%d1%80%d1%83%d0%b7%d0%b8%d1%8f-%d0%b4%d0%b5%d1%82%d0%b8/32328982.html",
-    title: "Echo Kavkaza: Children from Ukraine escape to Georgia",
-    publication: "Echo Kavkaza",
-    date: "2023-03-22",
+    url: "https://www.rferl.org/a/georgia-christmas-trees-europe-business/32105473.html",
+    title: "Georgia’s pine tree export industry",
+    publication: "RFE/RL",
+    date: "2023-12-10",
     image: "assets/video2.jpg"
   },
   {
-    url: "https://www.rferl.org/a/ukraine-russia-filtration-georgia/32122647.html",
-    title: "RFE/RL: Russian filtration and Georgia’s role",
-    publication: "RFE/RL",
-    date: "2022-11-08",
+    url: "https://www.ekhokavkaza.com/a/31863707.html",
+    title: "Voices from Georgia’s Russian influx",
+    publication: "Echo Kavkaza",
+    date: "2023-07-15",
     image: "assets/video3.jpg"
   },
   {
-    url: "https://www.rferl.org/a/georgia-christmas-trees-europe-business/32105473.html",
-    title: "Georgia’s Christmas tree export to Europe",
+    url: "https://www.rferl.org/a/ukraine-russia-filtration-georgia/32122647.html",
+    title: "Ukrainian deportees in Georgia",
     publication: "RFE/RL",
-    date: "2022-10-25",
+    date: "2023-05-18",
     image: "assets/video4.jpg"
   }
 ];
