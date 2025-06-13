5d744580d1cdd4b4d0346f4c68436e6649cfc17e
Tokmando <98414437+Tokmando@users.noreply.github.com>
2025-06-14 01:32:31 +0400

    Create videos.js

---
+++ data/videos.js
@@ -0,0 +1,30 @@
+const videos = [
+  {
+    url: "https://www.youtube.com/watch?v=Sge5Y-G8Jds&t=382s",
+    title: "Sky News: Georgia’s new president sparks protests",
+    publication: "Sky News",
+    date: "2024-05-20",
+    image: "assets/video1.jpg"
+  },
+  {
+    url: "https://www.ekhokavkaza.com/a/%d1%83%d0%ba%d1%80%d0%b0%d0%b8%d0%bd%d0%b0-%d1%80%d0%be%d1%81%d1%81%d0%b8%d1%8f-%d0%b3%d1%80%d1%83%d0%b7%d0%b8%d1%8f-%d0%b4%d0%b5%d1%82%d0%b8/32328982.html",
+    title: "Echo Kavkaza: Children from Ukraine escape to Georgia",
+    publication: "Echo Kavkaza",
+    date: "2023-03-22",
+    image: "assets/video2.jpg"
+  },
+  {
+    url: "https://www.rferl.org/a/ukraine-russia-filtration-georgia/32122647.html",
+    title: "RFE/RL: Russian filtration and Georgia’s role",
+    publication: "RFE/RL",
+    date: "2022-11-08",
+    image: "assets/video3.jpg"
+  },
+  {
+    url: "https://www.rferl.org/a/georgia-christmas-trees-europe-business/32105473.html",
+    title: "Georgia’s Christmas tree export to Europe",
+    publication: "RFE/RL",
+    date: "2022-10-25",
+    image: "assets/video4.jpg"
+  }
+];
