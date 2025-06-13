ae158389f41fb5521bc87d8c86575d028252793b
Tokmando <98414437+Tokmando@users.noreply.github.com>
2025-06-14 01:44:04 +0400

    Update articles.js

--- data/articles.js
+++ data/articles.js
@@ -14,52 +14,17 @@ const articles = [
     image: "assets/article2.jpg"
   },
   {
-    url: "https://www.intellinews.com/eu-georgia-relations-hit-a-new-low-as-tbilisi-warns-of-foreign-funded-power-grab-324409/",
-    title: "EU-Georgia ties hit new low",
-    publication: "IntelliNews",
-    date: "2024-05-15",
-    image: "assets/article3.jpg"
-  },
-  {
-    url: "https://taz.de/Politik-und-Fussball-in-Georgien/!6013564/",
-    title: "Politics and football in Georgia",
-    publication: "taz",
-    date: "2024-05-10",
-    image: "assets/article4.jpg"
-  },
-  {
-    url: "https://taz.de/Massenproteste-gegen-Agentengesetz/!6009224/",
-    title: "Mass protests erupt in Georgia",
-    publication: "taz",
-    date: "2024-05-03",
-    image: "assets/article5.jpg"
-  },
-  {
     url: "https://www.rferl.org/a/azerbaijan-journalists-georgia-safe-haven/32808012.html",
     title: "Azerbaijani journalists find haven in Georgia",
     publication: "RFE/RL",
     date: "2024-04-27",
-    image: "assets/article6.jpg"
-  },
-  {
-    url: "https://eurasianet.org/planned-russian-base-in-abkhazia-could-threaten-georgias-middle-corridor-dreams",
-    title: "Russian base plan threatens Georgia's corridor",
-    publication: "Eurasianet",
-    date: "2024-04-20",
-    image: "assets/article7.jpg"
-  },
-  {
-    url: "https://www.rferl.org/a/georgia-fentanyl-ecstasy-drugs-clubs-opiods-deaths/32084524.html",
-    title: "Drug crisis in Georgia's nightlife",
-    publication: "RFE/RL",
-    date: "2023-11-03",
-    image: "assets/article8.jpg"
+    image: "assets/article3.jpg"
   },
   {
     url: "https://eurasianet.org/south-ossetian-troops-fighting-for-russia-in-ukraine",
     title: "South Ossetians fight for Russia in Ukraine",
     publication: "Eurasianet",
     date: "2022-04-12",
-    image: "assets/article9.jpg"
+    image: "assets/article4.jpg"
   }
 ];
